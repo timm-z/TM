ba75b827d4eb224cf22ae38d67e82b1d8827283c
TimmZ <timm-z@web.de>
2013-08-13 12:53:07 +0200

    rake db:migrate + save posts / show post / list all posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,6 +3,23 @@ class PostsController < ApplicationController
 	end
 	
 	def create
-		render text: params[:post].inspect
+		@post = Post.new(params[:post].permit(:title, :text))
+
+		@post.save
+		redirect_to @post
+	end
+
+	def show
+		@post = Post.find(params[:id])
+		
+	end
+	
+	def index
+		@posts = Post.all
+	end
+	
+private
+	def post_params
+		params.require(:post).permit(:title, :text)
 	end
 end
