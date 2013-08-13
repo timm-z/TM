7b1b103f8c1d8c3b76ce8dbd3601d186d4fdbaac
TimmZ <timm-z@web.de>
2013-08-13 10:03:56 +0200

    rails new TM

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TM::Application.config.secret_key_base = '9ca31aab9f26dc4e1018213f9ed02bde2c4fe4aa95f9ada9e3e4c67a7b6552c97f866f1e4cf1e186a190ab0c9b0a2fc62dcfb98740c23c26931c7b699c501321'
