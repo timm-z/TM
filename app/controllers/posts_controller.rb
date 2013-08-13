f802c190e3c95091fc62ed9b39671d4755892caf
TimmZ <timm-z@web.de>
2013-08-13 13:14:34 +0200

    redirect failed saves to "new"-form
    render 'new' übergibt dabei variablen aus dem form. im gegensatz zu
    redirect_to

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,17 +1,20 @@
 class PostsController < ApplicationController
 	def new
+		@post = Post.new
 	end
-	
+ 
 	def create
 		@post = Post.new(params[:post].permit(:title, :text))
-
-		@post.save
-		redirect_to @post
+	 
+		if @post.save
+			redirect_to @post
+		else
+			render 'new'
+		end
 	end
 
 	def show
 		@post = Post.find(params[:id])
-		
 	end
 	
 	def index
